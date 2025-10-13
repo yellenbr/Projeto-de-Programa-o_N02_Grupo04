4edb65fcfe793d6e986d3be24a5e8a2a8ca773d0
Rayelen Oliveira <rayelenoliveiraa@gmail.com>
2025-10-13 20:55:48 -0300

    Add files via upload

---
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/PlataformacursosApplication.java
@@ -0,0 +1,13 @@
+package com.veridia.gestao.plataformacursos.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformacursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformacursosApplication.class, args);
+	}
+
+}
---
+++ plataformacursos/plataformacursos/src/test/java/com/veridia/gestao/plataformacursos/PlataformacursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veridia.gestao.plataformacursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformacursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
