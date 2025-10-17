f59574456655f8293f6d59dfba62f340781b107c
anasrmra <anasrmra@gmail.com>
2025-10-16 23:28:09 -0300

    Adição de pastas para Controller, Service e Repository, com classe Aluno inclusa em cada uma das pastas

---
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/Controller/AlunoController.java
@@ -0,0 +1,12 @@
+package com.veridia.gestao.plataformacursos.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+}
---
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/Service/AlunoService.java
@@ -0,0 +1,12 @@
+package com.veridia.gestao.plataformacursos.Service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AlunoService {
+
+    }
+
+
