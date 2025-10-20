70938ca2d0fdc0e939d3f9cc751400962d168600
sosoNote <anasrmra@gmail.com>
2025-10-20 19:46:39 -0300

    Ajuste nas pastas de Controller, Repository e Service da classe Aluno

--- plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/Repository/AlunoRepository.java
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/Repository/AlunoRepository.java
@@ -1,8 +1,9 @@
 package com.veridia.gestao.plataformacursos.Repository;
 
+import com.veridia.gestao.plataformacursos.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AlunoRepository {
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 }
--- plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/Service/AlunoService.java
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/Service/AlunoService.java
@@ -1,12 +1,33 @@
 package com.veridia.gestao.plataformacursos.Service;
 
+import com.veridia.gestao.plataformacursos.Aluno;
+import com.veridia.gestao.plataformacursos.Repository.AlunoRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AlunoService {
 
+    private final AlunoRepository alunoRepository;
+
+    public AlunoService(AlunoRepository alunoRepository) {
+        this.alunoRepository = alunoRepository;
     }
 
+    public List<Aluno> listarTodos() {
+        return alunoRepository.findAll();
+    }
 
+    public Optional<Aluno> buscarPorId(Long id) {
+        return alunoRepository.findById(id);
+    }
+
+    public Aluno salvar(Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    public void deletar(Long id) {
+        alunoRepository.deleteById(id);
+    }
+}
