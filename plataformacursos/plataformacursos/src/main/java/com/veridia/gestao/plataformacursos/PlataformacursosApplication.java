3bab41290bb58e921b9a719577ef4ae6c887cb74
Luan Gouveia <luangfdev@gmail.com>
2025-10-27 10:34:25 -0300

    ajuste model

--- plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/PlataformacursosApplication.java
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/PlataformacursosApplication.java
@@ -1,4 +1,4 @@
-package com.veridia.gestao.plataformacursos.model;
+package com.veridia.gestao.plataformacursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
