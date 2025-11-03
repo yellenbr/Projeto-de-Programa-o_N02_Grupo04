18b1d550382cc0f363a6ca78d6ea35bd43c4537f
Luan Gouveia Falcão dos Anjos <1241104619@acd.grupotiradentes.com>
2025-11-03 20:26:35 -0300

    alguns elementos a mais

--- plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/repository/InscricaoRepository.java
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/repository/InscricaoRepository.java
@@ -4,5 +4,19 @@ import com.veridia.gestao.plataformacursos.model.Inscricao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface InscricaoRepository extends JpaRepository<Inscricao, Long> {}
+public interface InscricaoRepository extends JpaRepository<Inscricao, Long> {
+
+    List<Inscricao> findByAluno_Id(Long alunoId);
+
+    List<Inscricao> findByCurso_Id(Long cursoId);
+
+    List<Inscricao> findByStatus(Inscricao.StatusInscricao status);
+
+    Optional<Inscricao> findByAluno_IdAndCurso_Id(Long alunoId, Long cursoId);
+
+    boolean existsByAluno_IdAndCurso_IdAndStatusNot(Long alunoId, Long cursoId, Inscricao.StatusInscricao status);
+}
