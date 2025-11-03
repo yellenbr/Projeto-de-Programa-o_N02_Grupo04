d47fc5385cffa32e24d273512ee7945ed075c732
Luan Gouveia Falcão dos Anjos <1241104619@acd.grupotiradentes.com>
2025-11-03 20:28:28 -0300

    Mais elementos

---
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/repository/CursoRepository.java
@@ -0,0 +1,15 @@
+package com.veridia.gestao.plataformacursos.repository;
+
+import com.veridia.gestao.plataformacursos.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+    List<Curso> findByNomeContainingIgnoreCase(String nome);
+
+    List<Curso> findByInstrutor_Id(Long instrutorId);
+}
