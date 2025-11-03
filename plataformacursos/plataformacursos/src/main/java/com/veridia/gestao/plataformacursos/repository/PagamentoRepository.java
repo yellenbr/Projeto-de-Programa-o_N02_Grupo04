91df5a39bc902d52f7542782d1146480062f078a
Luan Gouveia <luangfdev@gmail.com>
2025-11-03 18:06:01 -0300

    repository do pagamento

--- plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/repository/PagamentoRepository.java
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/repository/PagamentoRepository.java
@@ -4,8 +4,13 @@ import com.veridia.gestao.plataformacursos.model.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {}
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
 
+    Optional<Pagamento> findByInscricao_Id(Long inscricaoId);
 
+    List<Pagamento> findByStatus(Pagamento.StatusPagamento status);
+}
