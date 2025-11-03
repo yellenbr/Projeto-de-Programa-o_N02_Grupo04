70baa034d57580b90763f8debac350a564c7ee79
Luan Gouveia Falcão dos Anjos <1241104619@acd.grupotiradentes.com>
2025-11-03 20:29:08 -0300

    mais elementos

---
+++ plataformacursos/plataformacursos/src/main/java/com/veridia/gestao/plataformacursos/repository/AlunoRepository.java
@@ -0,0 +1,19 @@
+package com.veridia.gestao.plataformacursos.repository;
+
+import com.veridia.gestao.plataformacursos.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+    Optional<Aluno> findByEmail(String email);
+
+    Optional<Aluno> findByCpf(String cpf);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByCpf(String cpf);
+}
